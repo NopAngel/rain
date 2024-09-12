e728add0722a378cf816cdcd4b42ee9f742e8f31
NopAngel <angelgabrielnieto@outlook.com>
2024-09-12 19:43:12 -0400

    Add rain-editor: main editor

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rain Editor
+
+Rain, this is an editor very similar to VIM, but very simple, its interface is not the best but it can help you in several cases where you need to have your Urgent App. The download is in the download folder, or you can download it by clicking here!
+
+# RainX Language Programming.
--- main.cpp
+++ main.cpp
@@ -1,9 +1,95 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+void setColor(int textColor)
+{
+    std::cout << "\033[" << textColor << "m";
+}
+
+void resetColor()
+{
+    std::cout << "\033[0m";
+}
+
+void readFile(const std::string &filePath)
+{
+    std::ifstream file(filePath);
+    if (file)
+    {
+        std::string line;
+        std::cout << "File content:\n";
+        while (std::getline(file, line))
+        {
+            std::cout << line << std::endl;
+        }
+        file.close();
+    }
+    else
+    {
+        std::cout << "File '" << filePath << "' does not exist." << std::endl;
+    }
+}
+
+void writeFile(const std::string &filePath)
+{
+    std::ofstream file(filePath, std::ios::app);
+    if (file)
+    {
+        std::string input;
+        std::cout << "Enter text to write to the file (type 'exit' to stop):\n";
+        while (true)
+        {
+            std::getline(std::cin, input);
+            if (input == "exit")
+            {
+                std::cout << "\n\nGoodbye :D!";
+                break;
+            }
+            file << input << std::endl;
+        }
+        file.close();
+    }
+    else
+    {
+        std::cout << "Unable to open file '" << filePath << "' for writing." << std::endl;
+    }
+}
 
 int main()
 {
+    std::string res;
 
-    std::cout << "Hola, Mundo!\n";
+    do
+    {
+        setColor(31);
+        std::cout << "===================================================================\n";
+        resetColor();
+        std::cout << "                             Rain Editor                          \n";
+        std::cout << "\n\n";
+        std::cout << "                 !code - You could write code.                    \n";
+        std::cout << "          :github - You can see our GitHub repository             \n";
+        std::cout << "          rainn:@v - Show the Rain CLI version (Coming soon)      \n";
+        setColor(31);
+        std::cout << "===================================================================\n";
+        resetColor();
+        std::cin >> res;
+        if (res == ":github")
+        {
+            std::cout << "https://github.com/NopAngel/rain - Follow me and give it a Star :D!\n\n";
+        }
+
+    } while (res != "!code");
+
+    if (res == "!code")
+    {
+        std::string filePath;
+        std::cout << "Enter the file path: ";
+        std::cin >> filePath;
+
+        readFile(filePath);
+        writeFile(filePath);
+    }
 
     return 0;
 }
